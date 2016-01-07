d0cb85b6c8c6d7202074069624b04ebf99c0860e
camrun91 <cameron.b.llewellyn@gmail.com>
2016-01-07 16:24:24 -0600

    Revert "123"
    This reverts commit 004ba3c090c0a228d115c4695ff47a4431f0b69e.

--- ext_localconf.php
+++ ext_localconf.php
@@ -95,12 +95,12 @@ if (TYPO3_MODE === 'BE') {
 		'postFolderRename'
 	);
     // File tree icon adjustments for TYPO3 => 7.5
-   /** $signalSlotDispatcher->connect(
+    $signalSlotDispatcher->connect(
         'TYPO3\\CMS\\Core\\Imaging\\IconFactory',
         'buildIconForResourceSignal',
         'BeechIt\\FalSecuredownload\\Hooks\\IconUtilityHook',
         'buildIconForResource'
-    );*/
+    );
 
 	// ext:ke_search custom indexer hook
 	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyFileIndexEntryFromContentIndexer'][] = 'BeechIt\\FalSecuredownload\\Hooks\\KeSearchFilesHook';
