00e3a509b0f97bd263e550f5e3311a1d08bf876d
Frans Saris <frans@beech.it>
2016-04-21 15:48:19 +0200

    [TASK] Added base for unit tests

---
+++ Tests/Unit/Security/CheckPermissionsTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace BeechIt\FalSecuredownload\Tests\Unit\Security;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 21-04-2016
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * Class CheckPermissionsTest
+ */
+class CheckPermissionsTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function createCheckPermissionsObjectTest()
+    {
+
+    }
+}
