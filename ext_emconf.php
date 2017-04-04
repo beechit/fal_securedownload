fe1901cd9147dcc9f20895e534204eadaf26a738
Frans Saris <frans@beech.it>
2017-04-04 15:51:01 +0200

    [TASK] Compatible with TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.2.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.2 - 8.6.99',
+            'typo3' => '6.2.2 - 8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
