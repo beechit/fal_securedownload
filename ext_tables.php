ff17f8ceea4d78cd3e81cda91a2b330fa535f3ac
Markus Klein <markus.klein@reelworx.at>
2017-05-28 23:39:56 +0200

    [CLEANUP] Move tt_content manipulation into TCA override

--- ext_tables.php
+++ ext_tables.php
@@ -1,29 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'Filetree',
-    'LLL:EXT:fal_securedownload/Resources/Private/Language/locallang_be.xlf:plugin.title'
-);
-
-$pluginSignature = str_replace('_', '', $_EXTKEY) . '_filetree';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,recursive,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignature,
-    'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/FileTree.xml'
-);
-
-
-
 if (TYPO3_MODE === 'BE') {
     // Add click menu item:
     $GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = [
         'name' => 'BeechIt\\FalSecuredownload\\Hooks\\ClickMenuOptions'
     ];
 }
-
 
 // Initiate
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Imaging\\IconRegistry');
