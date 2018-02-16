1a3f6ecc29765b18a03b989d4074aed4dfe791fa
René Rossi <rossi@pixelproduction.de>
2018-02-16 09:12:45 +0100

    Filename without whitespace replacement and file extension checking.

--- Classes/Hooks/FileDumpHook.php
+++ Classes/Hooks/FileDumpHook.php
@@ -199,12 +199,7 @@ class FileDumpHook implements FileDumpEIDHookInterface
             $file->getStorage()->dumpFileContents($file, $asDownload);
             exit;
         }
-        $download_name = $file->getProperty('download_name') ? $file->getProperty('download_name') : $file->getName();
-        $file_parts = pathinfo($download_name);
-        if( $file_parts['extension'] == '' ){
-        	$download_name = preg_replace( ['/\W/'], ['_'], $download_name ).'.'.$file->getExtension(); // append the original extension
-        }
-
+        $downloadName = $file->getProperty('download_name') ?: $file->getName();
 
         $contentDisposition = $asDownload ? 'attachment' : 'inline';
         header('Content-Disposition: ' . $contentDisposition . '; filename="' . $download_name . '"');
