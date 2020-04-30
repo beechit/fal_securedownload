ec28940686fb119e331ad801070e9551937426cc
Joey Bouten <joey@beech.it>
2020-04-30 16:21:48 +0200

    [TASK] Remove unneeded vendor name
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.1/Deprecation-88995-CallingRegisterPluginWithVendorName.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'BeechIt.FalSecuredownload',
+    'FalSecuredownload',
     'Filetree',
     [
         'FileTree' => 'tree',
