6f79245d2e4e5fe96128dcd1f10d01461b8a85ef
Joey Bouten <joey@beech.it>
2020-08-03 08:40:12 +0200

    [TASK] Update minor version

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0 - 10.4.99',
+            'typo3' => '>=9.5.20 || >=10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
