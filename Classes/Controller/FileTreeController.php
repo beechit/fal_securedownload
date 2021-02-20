088c750f60378c7c532bfdc87780c039b778b5cf
beckerr-rzht <beckerr@hochschule-trier.de>
2021-02-20 13:13:56 +0100

    added { }

--- Classes/Controller/FileTreeController.php
+++ Classes/Controller/FileTreeController.php
@@ -52,8 +52,9 @@ class FileTreeController extends ActionController
      */
     public function treeAction()
     {
-        if ($this->settings['storage'] === '')
+        if ($this->settings['storage'] === '') {
             return;
+        }
         try {
             $folder = $this->resourceFactory->getFolderObjectFromCombinedIdentifier($this->settings['storage'] . ':' . $this->settings['folder']);
         } catch (FolderDoesNotExistException $exception) {
