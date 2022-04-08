32ea1211f169f58841eb344c967e0f57891f1ff2
Falko Linke <falkolinke@gmail.com>
2022-04-08 13:41:25 +0200

    port HttpUtility::setResponseCodeAndExit to responseFactory

--- Classes/Controller/BePublicUrlController.php
+++ Classes/Controller/BePublicUrlController.php
@@ -6,6 +6,8 @@ namespace BeechIt\FalSecuredownload\Controller;
  * Date: 22-08-2014 16:04
  * All code (c) Beech Applications B.V. all rights reserved
  */
+
+use Psr\Http\Message\ResponseFactoryInterface;
 use TYPO3\CMS\Core\Resource\ProcessedFile;
 use TYPO3\CMS\Core\Http\AbstractApplication;
 use TYPO3\CMS\Core\Resource\ProcessedFileRepository;
@@ -24,11 +26,18 @@ class BePublicUrlController extends AbstractApplication
     protected $resourceFactory;
 
     /**
-     * @param ResourceFactory $resourceFactory
+     * @var ResponseFactoryInterface
      */
-    public function __construct(ResourceFactory $resourceFactory = null)
+    protected ResponseFactoryInterface $responseFactory;
+
+    /**
+     * @param ResourceFactory|null $resourceFactory
+     * @param ResponseFactoryInterface $responseFactory
+     */
+    public function __construct(ResourceFactory $resourceFactory = null, ResponseFactoryInterface $responseFactory)
     {
         $this->resourceFactory = $resourceFactory ?? GeneralUtility::makeInstance(ResourceFactory::class);
+        $this->responseFactory = $responseFactory ?? GeneralUtility::makeInstance(ResponseFactoryInterface::class);
     }
 
     /**
@@ -63,18 +72,18 @@ class BePublicUrlController extends AbstractApplication
                 /** @var ProcessedFile $file */
                 $file = GeneralUtility::makeInstance(ProcessedFileRepository::class)->findByUid($parameters['p']);
                 if ($file->isDeleted()) {
-                    HttpUtility::setResponseCodeAndExit(HttpUtility::HTTP_STATUS_404);
+                    return $this->responseFactory->createResponse(404);
                 }
                 $orgFile = $file->getOriginalFile();
             }
 
             // Check file read permissions
             if (!$orgFile->getStorage()->checkFileActionPermission('read', $orgFile)) {
-                HttpUtility::setResponseCodeAndExit(HttpUtility::HTTP_STATUS_403);
+                return $this->responseFactory->createResponse(403);
             }
 
             if ($file === null) {
-                HttpUtility::setResponseCodeAndExit(HttpUtility::HTTP_STATUS_404);
+                return $this->responseFactory->createResponse(404);;
             }
 
             ob_start();
@@ -84,6 +93,6 @@ class BePublicUrlController extends AbstractApplication
 
             exit;
         }
-        HttpUtility::setResponseCodeAndExit(HttpUtility::HTTP_STATUS_403);
+        return $this->responseFactory->createResponse(403);
     }
 }
