64f0dffd44e160b1de9f5c0a870ddba637a129da
AlexKvrlp <31535838+AlexKvrlp@users.noreply.github.com>
2023-05-16 11:58:13 +0200

    Create JavaScriptModules.php
    Javascript AMD Modules are replaced by ES6 modules see https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/ApiOverview/Backend/JavaScript/ES6/Index.html#backend-javascript-es6

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dependencies' => ['backend'],
+    'tags' => [
+        'backend.contextmenu',
+    ],
+    'imports' => [
+        '@typo3/fal_securedownload/' => 'EXT:fal_securedownload/Resources/Public/JavaScript/',
+    ],
+];
