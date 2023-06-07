a786f1fd491a3d0986c5aa20b27b130f7aff242e
Stephan Großberndt <s.grossberndt@sidebysite.de>
2023-06-07 11:21:01 +0200

    Update CollapsibleFolderTree.js
    * Fix typo
    * Fix formatting

--- Resources/Public/JavaScript/CollapsibleFolderTree.js
+++ Resources/Public/JavaScript/CollapsibleFolderTree.js
@@ -1,20 +1,24 @@
-$('.fal-securedownload ul li span.icon-folder').on('click', function() {
-    var $leef = $(this).parent('li');
-    if ($('span.icon-folder:first', $leef).hasClass('icon-folder-open')) {
-        $leef.find('ul:first').slideUp('fast', function(){$('span.icon-folder:first', $leef).removeClass('icon-folder-open')});
-        $.get('/index.php?eID=FalSecuredownloadFileTreeState',{
-            folder: $('span.icon-folder:first', $leef).data('folder'),
+$('.fal-securedownload ul li span.icon-folder').on('click', function () {
+    var $leaf = $(this).parent('li');
+    if ($('span.icon-folder:first', $leaf).hasClass('icon-folder-open')) {
+        $leaf.find('ul:first').slideUp('fast', function () {
+            $('span.icon-folder:first', $leaf).removeClass('icon-folder-open')
+        });
+        $.get('/index.php?eID=FalSecuredownloadFileTreeState', {
+            folder: $('span.icon-folder:first', $leaf).data('folder'),
             open: ''
         });
     } else {
-        $leef.find('ul:first').slideDown('fast', function(){$('span.icon-folder:first', $leef).addClass('icon-folder-open')});
-        $.get('/index.php?eID=FalSecuredownloadFileTreeState',{
-            folder: $('span.icon-folder:first', $leef).data('folder'),
+        $leaf.find('ul:first').slideDown('fast', function () {
+            $('span.icon-folder:first', $leaf).addClass('icon-folder-open')
+        });
+        $.get('/index.php?eID=FalSecuredownloadFileTreeState', {
+            folder: $('span.icon-folder:first', $leaf).data('folder'),
             open: 1
         });
     }
 });
-$('.fal-securedownload ul li span.icon-folder').each(function() {
+$('.fal-securedownload ul li span.icon-folder').each(function () {
     $(this).next('ul').hide();
     if ($(this).hasClass('icon-folder-open')) {
         $(this).next('ul').slideDown();
