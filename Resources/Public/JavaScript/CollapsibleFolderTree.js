484e8681e6a34d48ea9be96e2cd6e518cb7d1f85
Stephan Großberndt <s.grossberndt@sidebysite.de>
2023-06-07 11:41:43 +0200

    Update .editorconfig and format accordingly

--- Resources/Public/JavaScript/CollapsibleFolderTree.js
+++ Resources/Public/JavaScript/CollapsibleFolderTree.js
@@ -1,25 +1,25 @@
 $('.fal-securedownload ul li span.icon-folder').on('click', function () {
-    var $leaf = $(this).parent('li');
-    if ($('span.icon-folder:first', $leaf).hasClass('icon-folder-open')) {
-        $leaf.find('ul:first').slideUp('fast', function () {
-            $('span.icon-folder:first', $leaf).removeClass('icon-folder-open')
-        });
-        $.get('/index.php?eID=FalSecuredownloadFileTreeState', {
-            folder: $('span.icon-folder:first', $leaf).data('folder'),
-            open: ''
-        });
-    } else {
-        $leaf.find('ul:first').slideDown('fast', function () {
-            $('span.icon-folder:first', $leaf).addClass('icon-folder-open')
-        });
-        $.get('/index.php?eID=FalSecuredownloadFileTreeState', {
-            folder: $('span.icon-folder:first', $leaf).data('folder'),
-            open: 1
-        });
-    }
+  var $leaf = $(this).parent('li');
+  if ($('span.icon-folder:first', $leaf).hasClass('icon-folder-open')) {
+    $leaf.find('ul:first').slideUp('fast', function () {
+      $('span.icon-folder:first', $leaf).removeClass('icon-folder-open')
+    });
+    $.get('/index.php?eID=FalSecuredownloadFileTreeState', {
+      folder: $('span.icon-folder:first', $leaf).data('folder'),
+      open: ''
+    });
+  } else {
+    $leaf.find('ul:first').slideDown('fast', function () {
+      $('span.icon-folder:first', $leaf).addClass('icon-folder-open')
+    });
+    $.get('/index.php?eID=FalSecuredownloadFileTreeState', {
+      folder: $('span.icon-folder:first', $leaf).data('folder'),
+      open: 1
+    });
+  }
 }).each(function () {
-    $(this).next('ul').hide();
-    if ($(this).hasClass('icon-folder-open')) {
-        $(this).next('ul').slideDown();
-    }
+  $(this).next('ul').hide();
+  if ($(this).hasClass('icon-folder-open')) {
+    $(this).next('ul').slideDown();
+  }
 });
--- Resources/Public/JavaScript/ContextMenuActions.js
+++ Resources/Public/JavaScript/ContextMenuActions.js
@@ -5,43 +5,43 @@
  * @exports TYPO3/CMS/FalSecuredownload/ContextMenuActions
  */
 define(function () {
-    'use strict';
+  'use strict';
 
-    /**
-     * @exports TYPO3/CMS/FalSecuredownload/ContextMenuActions
-     */
-    var ContextMenuActions = {};
+  /**
+   * @exports TYPO3/CMS/FalSecuredownload/ContextMenuActions
+   */
+  var ContextMenuActions = {};
 
-    /**
-     * Open folder permissions edit form
-     *
-     * @param {string} table
-     * @param {string} uid combined folder identifier
-     */
-    ContextMenuActions.folderPermissions = function (table, uid) {
-        var folderRecordUid = this.data('folderRecordUid') || 0;
+  /**
+   * Open folder permissions edit form
+   *
+   * @param {string} table
+   * @param {string} uid combined folder identifier
+   */
+  ContextMenuActions.folderPermissions = function (table, uid) {
+    var folderRecordUid = this.data('folderRecordUid') || 0;
 
-        if (folderRecordUid > 0) {
-            top.TYPO3.Backend.ContentContainer.setUrl(
-                top.TYPO3.settings.FormEngine.moduleUrl
-                + '&edit[tx_falsecuredownload_folder][' + parseInt(folderRecordUid, 10) + ']=edit'
-                + '&returnUrl=' + ContextMenuActions.getReturnUrl()
-            );
-        } else {
-            top.TYPO3.Backend.ContentContainer.setUrl(
-                top.TYPO3.settings.FormEngine.moduleUrl
-                + '&edit[tx_falsecuredownload_folder][0]=new'
-                + '&defVals[tx_falsecuredownload_folder][storage]=' + this.data('storage')
-                + '&defVals[tx_falsecuredownload_folder][folder]=' + this.data('folder')
-                + '&defVals[tx_falsecuredownload_folder][folder_hash]=' + this.data('folderHash')
-                + '&returnUrl=' + ContextMenuActions.getReturnUrl()
-            );
-        }
-    };
+    if (folderRecordUid > 0) {
+      top.TYPO3.Backend.ContentContainer.setUrl(
+        top.TYPO3.settings.FormEngine.moduleUrl
+        + '&edit[tx_falsecuredownload_folder][' + parseInt(folderRecordUid, 10) + ']=edit'
+        + '&returnUrl=' + ContextMenuActions.getReturnUrl()
+      );
+    } else {
+      top.TYPO3.Backend.ContentContainer.setUrl(
+        top.TYPO3.settings.FormEngine.moduleUrl
+        + '&edit[tx_falsecuredownload_folder][0]=new'
+        + '&defVals[tx_falsecuredownload_folder][storage]=' + this.data('storage')
+        + '&defVals[tx_falsecuredownload_folder][folder]=' + this.data('folder')
+        + '&defVals[tx_falsecuredownload_folder][folder_hash]=' + this.data('folderHash')
+        + '&returnUrl=' + ContextMenuActions.getReturnUrl()
+      );
+    }
+  };
 
-    ContextMenuActions.getReturnUrl = function () {
-        return encodeURIComponent(top.list_frame.document.location.pathname + top.list_frame.document.location.search);
-    };
+  ContextMenuActions.getReturnUrl = function () {
+    return encodeURIComponent(top.list_frame.document.location.pathname + top.list_frame.document.location.search);
+  };
 
-    return ContextMenuActions;
+  return ContextMenuActions;
 });
